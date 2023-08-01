da3b79d04c017bd802a7c927cec0127582d80f4e
Jon Sundin <sundinjon@gmail.com>
2023-07-31 22:28:20 -0700

    pages index, app, document, and root layout

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+"use client";
+
+import { Fragment } from "react";
+import ZoomableCirclePacking from "@/components/ZoomableCirclePacking";
+
+function HomePage() {
+  return <ZoomableCirclePacking />;
+}
+
+export default HomePage;
