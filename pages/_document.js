caa107c6dda741ee42c16eb8549c75752ac12e0b
Jon Sundin <sundinjon@gmail.com>
2023-08-05 18:36:33 -0700

    moved analytics scripts into Head

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document({ children }) {
   return (
     <Html lang="en">
-      <Head />
-      <body className="bg-gradient-to-r from-indigo-950 to-indigo-500">
+      <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          strategy="afterInteractive"
+          src={
+            "https://www.googletagmanager.com/gtag/js?id=G-" +
+            process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+          }
+        ></Script>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+          
+            gtag('config', 'G-YF466VQK9Z');
+            `,
+          }}
+        />
+      </Head>
+      <body className="box-border flex flex-col bg-gradient-to-r from-indigo-950 to-indigo-500">
         <Main />
         <NextScript />
       </body>
