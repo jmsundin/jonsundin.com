d0f9193914e3d2560e8db5c16523d3ddd646ebc3
Jon Sundin <sundinjon@gmail.com>
2023-08-05 18:38:25 -0700

    added hamburger menu

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -1,10 +1,15 @@
 "use client";
 
-import * as React from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
+
 import Link from "next/link";
 
-import { cn } from "@/lib/utils";
-// import { Icons } from "@/components/icons";
+import { BsLinkedin } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -21,73 +26,165 @@ const navComponents = [
     description: "About Me through Information Visualization",
     href: "/about-me-with-d3",
   },
-  {
-    title: "Resume",
-    description: "My Resume",
-    href: "/resume",
-  },
-  {
-    title: "Personal Projects",
-    description: "My Personal Projects",
-    href: "/personal-projects",
-  },
-  {
-    title: "Professional Projects",
-    description: "My Professional Projects",
-    href: "/professional-projects",
-  },
+  // {
+  //   title: "Resume",
+  //   description: "My Resume",
+  //   href: "/resume",
+  // },
+  // {
+  //   title: "Personal Projects",
+  //   description: "My Personal Projects",
+  //   href: "/personal-projects",
+  // },
+  // {
+  //   title: "Professional Projects",
+  //   description: "My Professional Projects",
+  //   href: "/professional-projects",
+  // },
 ];
 
 export default function MainNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  const showModalMenu = () => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+      return;
+    }
+    setIsMenuOpen(true);
+  };
+
   return (
-    <div className="flex flex-row items-center justify-center w-full px-2">
-      <Link href="/" passHref>
-        <h1 className="scroll-m-20 text-4xl font-bold hover:font-extrabold tracking-tight lg:text-5xl">
-          Jon Sundin
-        </h1>
-      </Link>
-      <NavigationMenu className="mx-auto my-2 p-2 border border-solid-4 border-gray-500 rounded-xl">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>About Me</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                {navComponents.map((component) => (
-                  <li key={component.href}>
-                    <NavigationMenuLink asChild>
-                      <Link
-                        href={component.href}
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md hover:bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                      >
-                        <div className="mb-2 text-lg font-medium">
-                          {component.title}
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          {component.description}
-                        </p>
-                      </Link>
-                    </NavigationMenuLink>
-                  </li>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/contact" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Contact
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
+    <Fragment>
+      <div className="flex flex-row p-2 justify-between items-center">
+        <Link
+          href="/"
+          legacyBehavior
+          passHref
+          className="flex flex-initial items-start"
+        >
+          <h1 className="text-3xl text-gray-200 font-bold hover:text-indigo-200 cursor-pointer tracking-tight lg:text-5xl">
+            Jon Sundin
+          </h1>
+        </Link>
+        <NavigationMenu className="hidden sm:inline-block sm:mx-auto sm:my-2 sm:p-2 sm:border sm:border-solid-4 sm:border-gray-500 sm:rounded-xl">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>About Me</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  {navComponents.map((component) => (
+                    <li key={component.href}>
+                      <NavigationMenuLink asChild>
+                        <Link
+                          href={component.href}
+                          className="flex h-full w-full select-none flex-col justify-end rounded-md hover:bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        >
+                          <div className="mb-2 text-lg font-medium">
+                            {component.title}
+                          </div>
+                          <p className="text-sm leading-tight text-muted-foreground">
+                            {component.description}
+                          </p>
+                        </Link>
+                      </NavigationMenuLink>
+                    </li>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/contact" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Contact
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        {/* Social Media Links */}
+        <div className="flex flex-row w-50 mx-auto gap-5 text-white text-base sm:text-2xl">
+          <Link
+            href="https://www.linkedin.com/in/jonsundin/"
+            target="_blank"
+            className="flex flex-1"
+          >
+            <span className="text-white hover:text-indigo-200">
+              <BsLinkedin />
+            </span>
+          </Link>{" "}
+          <Link
+            href="https://www.twitter.com/jonsundin/"
+            target="_blank"
+            className="flex flex-1"
+          >
+            <span className="text-white hover:text-indigo-200">
+              <BsTwitter />
+            </span>
+          </Link>{" "}
+          <Link
+            href="https://github.com/jmsundin"
+            target="_blank"
+            className="flex flex-1"
+          >
+            <span className="text-white hover:text-indigo-200">
+              <BsGithub />
+            </span>
+          </Link>{" "}
+        </div>
+
+        {/* Mobile Menu */}
+        {!isMenuOpen && (
+          <RiMenu3Line
+            className="relative text-white text-3xl cursor-pointer hover:fill-indigo-200 sm:hidden"
+            onClick={showModalMenu}
+          />
+        )}
+        {isMenuOpen && (
+          <div
+            ref={modalRef}
+            className="absolute top-0 right-0 z-50 flex flex-col w-full h-full p-2 bg-gradient-to-r from-indigo-950 to-indigo-500"
+          >
+            <div className="flex flex-row justify-end">
+              <RiCloseLine
+                className="flex flex-row text-white text-4xl cursor-pointer hover:fill-indigo-200 sm:hidden"
+                onClick={showModalMenu}
+              />
+            </div>
+            <div
+              className="flex flex-row w-full justify-center"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <div className="flex flex-col w-1/2 justify-center items-center gap-4">
+                <Link href="/" legacyBehavior passHref>
+                  <p className="font-bold text-2xl text-gray-200 p-2 cursor-pointer hover:text-indigo-200 ">
+                    Home
+                  </p>
+                </Link>
+                <Link href="/about-me-with-d3" legacyBehavior passHref>
+                  <p className="font-bold text-2xl text-gray-200 p-2 cursor-pointer hover:text-indigo-200">
+                    About Me
+                  </p>
+                </Link>
+                <Link href="/contact" legacyBehavior passHref className="">
+                  <p className="font-bold text-2xl text-gray-200 p-2 cursor-pointer hover:text-indigo-200">
+                    Contact
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </Fragment>
   );
 }
