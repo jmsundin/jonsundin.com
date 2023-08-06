36db33238908bced0cc6d034d023a92dce8b18e4
Jon Sundin <sundinjon@gmail.com>
2023-08-06 16:54:58 -0700

    added allowCors wrapper

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -1,23 +1,20 @@
-import Cors from "cors";
 import sendgrid from "@sendgrid/mail";
 
-// Initializing the cors middleware
-// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-const cors = Cors({
-  methods: ["POST"],
-});
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
 }
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
@@ -71,4 +68,4 @@ async function handler(req, res) {
   }
 }
 
-export default handler;
+module.exports = allowCors(handler);
