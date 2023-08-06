c6ba3f24eb59f03d15c1a8458a3d2189c6bc6905
Jon Sundin <sundinjon@gmail.com>
2023-08-05 18:36:49 -0700

    added Fragment

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
+
+import { Fragment } from "react";
 import RootLayout from "./layout";
 
 function App({ Component, pageProps }) {
   return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
+    <Fragment>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </Fragment>
   );
 }
 
