ecbd669f069c0ff7f49c4a598fbfc6fb6f0e54dd
Jon Sundin <sundinjon@gmail.com>
2023-08-07 14:47:03 -0700

    debugging method post error

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -1,4 +1,4 @@
-// import sendgrid from "@sendgrid/mail";
+import sendgrid from "@sendgrid/mail";
 
 const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
@@ -20,61 +20,54 @@ const allowCors = (fn) => async (req, res) => {
   return await fn(req, res);
 };
 
-// sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-// async function handler(req, res) {
-//   if (req.method === "POST") {
-//     const requestBody = req.body;
-//     console.log(requestBody);
-//     const msg = {
-//       to: "jon@infoverse.ai",
-//       from: "jon@infoverse.ai",
-//       subject: requestBody.subject,
-//       text: requestBody.message,
-//       html: `
-//       <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-//       <html lang="en">
-//       <body>
-//         <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
-//               </div>
-//               <div class="container" style="margin-left: 20px;margin-right: 20px;">
-//               <h3>From: ${requestBody.email}</h3>
-//               <div style="font-size: 16px;">
-//               <p>Subject: ${requestBody.subject}</p>
-//               <p><strong>Message:</strong></p>
-//               <p>${requestBody.message}</p>
-//               <br>
-//               </div>
-//               </div>
-//               </div>
-//       </body>
-//       </html>`,
-//     };
-
-//     try {
-//       await sendgrid.send(msg);
-//     } catch (error) {
-//       console.error(error);
-//       return res
-//         .status(error.statusCode || 500)
-//         .json({ status: error.statusCode, message: error.message });
-//     }
-
-//     res.status(200).json({
-//       status: "OK",
-//       message: `Email sent successfully, ${requestBody.name}!`,
-//     });
-//   } else {
-//     res.status(405).json({ message: "Method not allowed" });
-//   }
-// }
-const handler = (req, res) => {
+async function handler(req, res) {
+  console.log(req.method);
   if (req.method === "POST") {
-    res.status(200).json({ status: res.status, message: req.body.message });
-    res.end();
+    const requestBody = req.body;
+    console.log(requestBody);
+    const msg = {
+      to: "jon@infoverse.ai",
+      from: "jon@infoverse.ai",
+      subject: requestBody.subject,
+      text: requestBody.message,
+      html: `
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>From: ${requestBody.email}</h3>
+              <div style="font-size: 16px;">
+              <p>Subject: ${requestBody.subject}</p>
+              <p><strong>Message:</strong></p>
+              <p>${requestBody.message}</p>
+              <br>
+              </div>
+              </div>
+              </div>
+      </body>
+      </html>`,
+    };
+
+    try {
+      await sendgrid.send(msg);
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(error.statusCode || 500)
+        .json({ status: error.statusCode, message: `${error.message} line 61 error`, body: { ...error.body } });
+    }
+
+    res.status(200).json({
+      status: "OK",
+      message: `Email sent successfully, ${requestBody.name}!`,
+    });
   } else {
-    res.status(405).json({ message: "Method not allowed" });
+    res.status(405).json({ message: "Line 69 error" });
   }
-};
+}
 
 module.exports = allowCors(handler);
