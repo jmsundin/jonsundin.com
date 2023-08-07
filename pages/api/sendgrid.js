ed59442a422925690d54afe3770788e180c75e68
Jon Sundin <sundinjon@gmail.com>
2023-08-06 18:03:19 -0700

    changed options to post

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -13,7 +13,7 @@ const allowCors = (fn) => async (req, res) => {
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
-  if (req.method === "OPTIONS") {
+  if (req.method === "POST") {
     res.status(200).end();
     return;
   }
@@ -69,7 +69,7 @@ const allowCors = (fn) => async (req, res) => {
 //   }
 // }
 const handler = (req, res) => {
-  res.status(200).json({ stats: res.status, message: "Hello" });
+  res.status(200).json({ stats: res.status, message: req.body });
   res.end();
 };
 
