17bc3b61694c5ab6d7fc7801f4566244f1f64d19
Jon Sundin <sundinjon@gmail.com>
2023-08-09 20:12:39 -0700

    removing div, footer is positioned relative main

--- pages/layout.js
+++ pages/layout.js
@@ -4,10 +4,10 @@ import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <div className="relative flex flex-col min-h-screen">
+    <Fragment>
       <MainNav />
-      <main className="mb-20">{children}</main>
+      <main className="relative mb-20">{children}</main>
       <Footer />
-    </div>
+    </Fragment>
   );
 }
