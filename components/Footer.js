d337fd6b67bee8cda36ffd155055305c61d446bf
Jon Sundin <sundinjon@gmail.com>
2023-10-05 16:45:51 -0700

    fixed positioning to bottom

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import { RiCopyrightLine } from "react-icons/ri";
 
 function Footer() {
   return (
-    <footer className="absolute left-0 right-0">
-      <div className="flex flex-row justify-center text-white p-4">
+    <footer className="absolute bottom-0 left-0 right-0 min-h-[50px]">
+      <div className="flex flex-row justify-center text-white">
         <div className="bg-gradient-to-r from-gray-500 to-white h-0.5 w-5/6 mx-auto">
           {" "}
           <p className="flex gap-2 justify-center items-center py-3">
